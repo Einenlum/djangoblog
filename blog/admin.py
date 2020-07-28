84a5fe20534328a69b6b90f84d9d741800f79f95
Einenlum <yann.rabiller@gmail.com>
2020-07-28 12:54:56 +0200

    Add article model

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from .models import Article, User
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('author', 'published_at', 'title', 'slug')
+    exclude = ('published_at', 'slug')
+    
 
 admin.site.register(User, UserAdmin)
+admin.site.register(Article, ArticleAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,22 @@
+from datetime import datetime
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.template.defaultfilters import slugify
+
 
 class User(AbstractUser):
     pass
+
+
+class Article(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='articles')
+    content = models.TextField()
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True)
+    published_at = models.DateTimeField(default=datetime.now)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+
+        return super().save(*args, **kwargs)
