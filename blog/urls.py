d142b573a4c2cf46a4522e8b4dbb3aed01a659a6
Einenlum <yann.rabiller@gmail.com>
2020-07-29 15:49:12 +0200

    Add article creation

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('articles/create', views.ArticleCreate.as_view(), name='article_create'),
     path('articles/<article_slug>', views.article_show, name='article_show'),
     path('categories', views.category_index, name='category_index'),
     path('categories/<pk>', views.category_show, name='category_show'),
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.core.paginator import Paginator
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponseNotFound, HttpRequest
 from django.urls import reverse_lazy
 from django.views.defaults import page_not_found
@@ -44,6 +44,19 @@ def handler404(request: HttpRequest, exception, template_name="error_404.html"):
     response = render(request, template_name)
     response.status_code = 404
     return response
+
+
+class ArticleCreate(generic.CreateView):
+    model = Article
+    fields = ('title', 'content', 'categories')
+    template_name = 'article_create.html'
+
+    def form_valid(self, form):
+        article = form.save(commit=False)
+        article.author = self.request.user
+        article.save()
+
+        return redirect(article.get_absolute_url)
 
 
 class Signup(generic.CreateView):
